fb52a949b8e1874f5a0bd76182bc451cfa392a3c
sbubu12 <shiho.yokoi@yugudo.com>
2020-10-26 16:38:00 +0900

    first commitです

---
+++ Assets/Tile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Material redMaterial;
+    Material defaultMaterial;
+
+    Renderer renderer;
+
+    private void Awake()
+    {
+        renderer = GetComponent<Renderer>();
+        defaultMaterial = renderer.material;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("自分の色を変える");
+        renderer.material = redMaterial;
+
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("自分の色を戻す");
+        renderer.material = defaultMaterial;
+    }
+}
